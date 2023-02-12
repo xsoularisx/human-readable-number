ce92ec856c7af2fec55a3ee9df1bf330f85a7bb8
albert <xsoularisx@gmail.com>
2023-02-12 15:21:59 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-module.exports = function toReadable (number) {
-  if (number = 0) return 'zero'
+module.exports = function toReadable(number) {
+    const firstNumbers = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+    const dozens = ["", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    if (number === 0) {
+        return 'zero'
+    }
+
+    let getDozens = function (incNum) {
+        let number = Number.parseInt(incNum)
+        if (number < 20) {
+            if (number === 0) {
+                return ''
+            }
+            return firstNumbers[number]
+        } else if (number < 100) {
+            let newNum = number.toString().split('');
+            return `${dozens[newNum[0]]} ${firstNumbers[newNum[1]]}`
+        } else {
+            let newNum = number.toString().split('');
+            return `${firstNumbers[newNum[0]]} hundred ${getDozens(newNum[1] + newNum[2])}`
+        }
+    }
+    return getDozens(number).trimEnd()
 }
